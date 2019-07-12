6cd511e86d7c82984218782bce3f258ac2c0e969
Jasmine <jasminecharles@hotmail.com>
2019-07-12 09:30:24 -0700

    WIP

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,11 +7,33 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-container component
+
+const headerContainer = document.querySelector('.header-container')
+const element = Header()
+headerContainer.appendChild(element)
 
 function Header() {
+//Created elements
  const header = document.createElement('div');
  const date = document.createElement('span');
  const title = document.createElement('h1');
  const temp = document.createElement('span');
+
+ // Added structure to elements
+ header.appendChild(date)
+ date.appendChild(title)
+ date.appendChild(temp)
+
+ // Added classes
+ header.classList.add('header')
+ date.classList.add('date')
+ temp.classList.add('temp')
+
+ // Added text
+ date.textContent = 'MARCH 28, 2019'
+ title.textContent = 'Lambda Times'
+ temp.textContent = '98°'
+
+ return header
 }
